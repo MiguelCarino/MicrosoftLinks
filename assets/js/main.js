a9b4921aa1b292c12dfeb012812874ed6995859c
MiguelCarino <miguel.carino1994@outlook.com>
2024-11-18 01:17:10 -0600

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,7 +101,7 @@ fetch('assets/json/solutions.json')
                 }
 
                 var serviceLinux = document.createElement('i');
-                serviceLinux.className = service.linux || 'fab fa-fedoraup'; // Default to Linux icon if not provided
+                serviceLinux.className = service.linux || 'fab fa-fedora'; // Default to Linux icon if not provided
                 serviceLinux.dataset.value = "sudo dnf install " + service.linuxcmd; // Set the value to copy from JSON
                 if (service.linuxcmd) { // Only add click event if linuxcmd is present
                     serviceLinux.addEventListener('click', function(event) {
